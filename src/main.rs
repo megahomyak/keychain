55fb032eff82cc81e6ec4cb89ecfcc6e663e3ef4
megahomyak <g.megahomyak@gmail.com>
2025-03-09 00:28:59 +0500

    GOING FORWARD

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,81 @@
-enum Key {
-    RSA { key: String },
+use base64::Engine;
+use std::io::{Read, Write};
+
+struct Keychains {
+    secret: String,
+    public: String,
 }
 
-struct Keychain {
-    keys: Vec<Key>,
+fn join_keys(chain: Vec<Vec<String>>) -> String {
+    chain.into_iter().flatten().collect::<Vec<_>>().join(":")
 }
 
-enum ParsingError {
-    UnknownAlgo { name: String },
-    RSAKeyMissing,
-}
-
-impl std::str::FromStr for Keychain {
-    type Err = ParsingError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut items = s.split(':');
-        let mut keys = Vec::new();
-        while let Some(algo_name) = items.next() {
-            match algo_name {
-                "rsa" => {
-                    let key = items.next().ok_or(ParsingError::RSAKeyMissing)?;
-                    keys.push(Key::RSA { key: key.to_owned() });
-                }
-                _ => {
-                    return Err(ParsingError::UnknownAlgo {
-                        name: algo_name.to_owned(),
-                    })
-                }
-            }
-        }
-        Ok(Self { keys })
+fn make_keychains() -> Keychains {
+    let mut secret: Vec<Vec<String>> = Vec::new();
+    let mut public: Vec<Vec<String>> = Vec::new();
+    // Do all the key insertion thing
+    public.reverse();
+    Keychains {
+        secret: join_keys(secret),
+        public: join_keys(public),
     }
 }
-impl std::fmt::Display for Keychain {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (index, key) in self.keys.iter().enumerate() {
-            if index != 0 {
-                f.write_str(":")?;
-            }
-            match key {
-                Key::RSA { key } => {
-                    f.write_str("rsa:")?;
-                    f.write_str(key)?;
-                }
-            }
-        }
-        Ok(())
-    }
+
+fn enc(
+    mut input: Vec<u8>,
+    self_secret_keychain: String,
+    recipient_public_keychain: String,
+) -> Vec<u8> {
+}
+
+fn dec(
+    mut input: Vec<u8>,
+    self_secret_keychain_path: String,
+    recipient_public_keychain_path: String,
+) -> Vec<u8> {
 }
 
 fn main() {
-    println!("Hello, world!");
+    fn read(fpath: &str) -> String {
+        std::fs::read_to_string(fpath).unwrap()
+    }
+    let mut args = std::env::args();
+    let command = args.next().unwrap();
+    if command == "enc" {
+        let self_secret_keychain_path = args.next().unwrap();
+        let recipient_public_keychain_path = args.next().unwrap();
+        let mut input = Vec::new();
+        std::io::stdin().read_to_end(&mut input).unwrap();
+        let output = enc(
+            input,
+            read(&self_secret_keychain_path),
+            read(&recipient_public_keychain_path),
+        );
+        let output = base64::engine::general_purpose::STANDARD.encode(output);
+        println!("{}", output);
+    } else if command == "dec" {
+        let self_secret_keychain_path = args.next().unwrap();
+        let recipient_public_keychain_path = args.next().unwrap();
+        let mut input = Vec::new();
+        std::io::stdin().read_to_end(&mut input).unwrap();
+        let input = String::from_utf8(input).unwrap();
+        let input = input.trim();
+        let input = base64::engine::general_purpose::STANDARD
+            .decode(input)
+            .unwrap();
+        let output = dec(
+            input,
+            read(&self_secret_keychain_path),
+            read(&recipient_public_keychain_path),
+        );
+        std::io::stdout().write_all(&output).unwrap();
+    } else if command == "make_keychains" {
+        let self_secret_keychain_path = args.next().unwrap();
+        let self_public_keychain_path = args.next().unwrap();
+        let keychains = make_keychains();
+        std::fs::write(self_secret_keychain_path, keychains.secret).unwrap();
+        std::fs::write(self_public_keychain_path, keychains.public).unwrap();
+    } else {
+        eprintln!("Unknown command");
+    }
 }
